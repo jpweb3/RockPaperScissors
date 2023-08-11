c821739840cbc14ef9d64de3c81a465cd45270ca
jason parker <jas.parker@yahoo.com>
2023-08-11 12:10:33 -0400

    Created initial game lgic

---
+++ RockPaperScissors.py
@@ -0,0 +1,38 @@
+import random
+
+
+print("Welcome to Rock Paper Sicssors! ")
+
+user_choice = input("Please make a selection (rock, paper, or scissors): ")
+
+possible_choices = ["rock", "paper", "scissors"]
+computer_choice = random.choice(possible_choices)
+
+print(f"\n Your selection was {user_choice},  Your opponent's selection was {computer_choice}")
+
+if  user_choice == computer_choice:
+    print(f"Both players chose {user_choice}, tie game")
+#Make it so if its a tie both players choose again
+elif user_choice == "paper":
+    if computer_choice  == "rock":
+        print("Paper covers Rock! You Win!")
+    else:
+        print("Scissors cuts paper, you lose")
+elif user_choice == "rock":
+    if computer_choice == "scissors":
+        print("Rock smashes scissors! You Win!")
+    else:
+        print("Paper covers rock, you lose")
+elif user_choice == "scissors":
+    if computer_choice == "paper":
+        print("Scissors cuts paper, you win")
+    else:
+        print("Rock smashes scissors, you lose")
+
+
+
+    
+
+
+
+
